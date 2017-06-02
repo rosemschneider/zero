72565bea825ccbabecf07b8231f14f46f88f65c5
rschneid <rschneid@yalm01.psych.upenn.edu>
2017-06-02 16:05:02 -0400

    some minor changes, need to add subprocess for parser

--- textSearch.py
+++ textSearch.py
@@ -12,14 +12,21 @@ def find_sents():
     #pull out the lines corresponding to the word you're querying
     import re
     word_occur = []
-    pattern = re.compile('zero', re.IGNORECASE)
-    with open ('input.txt', 'rt', encoding = 'latin1') as in_file:
+    pattern = re.compile(substr, re.IGNORECASE)
+    with open (fname, 'rt', encoding = 'latin1') as in_file:
         for linenum, line in enumerate(in_file):
             if pattern.search(line) !=None:
                 word_occur.append((line.rstrip('\n')))
+   
+    filename = (substr + '_search.txt')
     
     import numpy
-    numpy.savetxt('test1.txt', word_occur, fmt = '%s')
+    numpy.savetxt(filename, word_occur, fmt = '%s')
+    #to do:
+        #figure out subprocess so I can launch the parser from here
+        
+    
+    
         
  
               
