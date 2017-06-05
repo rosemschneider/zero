a75edf7d9b95a00bcaa7f50d92c20299d5f92c10
rschneid <rschneid@yalm01.psych.upenn.edu>
2017-06-05 11:36:03 -0400

    created parsing script to find and parse sentences for word

--- textSearch.py
+++ textSearch.py
@@ -3,6 +3,8 @@
 #read in the text file
 
 def find_sents():
+    import os
+    os.chdir('/Users/rschneid/Documents/Projects/zero/stanford-parser-full-2016-10-31/')
     print("Please enter your substring")
     substr = input()
     print('Please enter your fileid')
@@ -18,12 +20,48 @@ def find_sents():
             if pattern.search(line) !=None:
                 word_occur.append((line.rstrip('\n')))
    
+    
+    #save it to text
     filename = (substr + '_search.txt')
     
     import numpy
     numpy.savetxt(filename, word_occur, fmt = '%s')
-    #to do:
-        #figure out subprocess so I can launch the parser from here
+    
+
+def parse_sents():
+    #this is a function for finding and automatically parsing text using stanford nlp
+    import os
+    os.chdir('/Users/rschneid/Documents/Projects/zero/stanford-parser-full-2016-10-31/')
+    
+    #get user input
+    print('Please enter your substring')
+    substr = input()
+    print('Please enter your fileid')
+    fname = input()
+    
+    ##pull out the lines corresponding to the word you're querying
+    import re
+    word_occur = []
+    pattern = re.compile(substr, re.IGNORECASE)
+    with open (fname, 'rt', encoding = 'latin1') as in_file:
+        for linenum, line in enumerate(in_file):
+            if pattern.search(line) !=None:
+                word_occur.append((line.rstrip('\n')))
+    
+    #save it to text file
+    filename = (substr + '_search.txt')
+    parsename = (substr + '_parse.txt')
+    
+    import numpy
+    numpy.savetxt(filename, word_occur, fmt = '%s')
+    
+    #make some variables to make running os.system easier
+    cmd = ('./lexparser.sh ' + filename + ' > ' + parsename + ' 2>&1')
+    
+    os.system(cmd)
+    
+            
+    
         
     
     
