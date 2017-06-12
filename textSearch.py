f22d65c7276d809ef750dc8980f3dd2643e199b6
rschneid <rschneid@yalm01.psych.upenn.edu>
2017-06-12 13:03:24 -0400

    fixed and streamlined things, everything works now

--- textSearch.py
+++ textSearch.py
@@ -1,16 +1,7 @@
 ##script for pulling out text from yang clean corpus
 
 #read in the text file
-
-def find_sents():
-    import os
-    os.chdir('/Users/rschneid/Documents/Projects/zero/parse_tag')
-    print("Please enter your substring")
-    substr = input()
-    print('Please enter your fileid')
-    fname = input()
-    type(fname)
-                    
+def find_sents(fname, substr):
     #pull out the lines corresponding to the word you're querying
     import re
     word_occur = []
@@ -19,86 +10,42 @@ def find_sents():
         for linenum, line in enumerate(in_file):
             if pattern.search(line) !=None:
                 word_occur.append((line.rstrip('\n')))
-   
-    
+     
     #save it to text
-    filename = ('../parse_tag_out/' + substr + '_search.txt')
+    filename = ('parse_tag_out/' + substr + '_search.txt')
     
     import numpy
     numpy.savetxt(filename, word_occur, fmt = '%s')
     
 
-def parse_sents():
-    #this is a function for finding and automatically parsing text using stanford nlp
-    import os
-    os.chdir('/Users/rschneid/Documents/Projects/zero/parse_tag')
+def parse_sents(fname, substr):
+    find_sents(fname, substr)
     
-    #get user input
-    print('Please enter your substring')
-    substr = input()
-    print('Please enter your fileid')
-    fname = input()
-    
-    ##pull out the lines corresponding to the word you're querying
-    import re
-    word_occur = []
-    pattern = re.compile(substr, re.IGNORECASE)
-    with open (fname, 'rt', encoding = 'latin1') as in_file:
-        for linenum, line in enumerate(in_file):
-            if pattern.search(line) !=None:
-                word_occur.append((line.rstrip('\n')))
-    
-    #save it to text file
-    filename = ('../parse_tag_out/' + substr + '_search.txt')
-    parsename = ('../parse_tag_out/' + substr + '_parse.txt')
-    
-    import numpy
-    numpy.savetxt(filename, word_occur, fmt = '%s')
+    parsename = ('parse_tag_out/' + substr + '_parse.txt')
     
     #make some variables to make running os.system easier
-    cmd = ('./lexparser.sh ' + filename + ' > ' + parsename + ' 2>&1')
-    
+    cmd = ('parse_tag/parse/./lexparser.sh ' + 'parse_tag_out/' + substr + '_search.txt'
+           + ' > ' + parsename + ' 2>&1')
+    import os
     #actually run command - this is running the parser in the command line
     #this is also saving the results of the parse as a txt file in the cwd
     os.system(cmd)
     
-def tag_sents(): 
-    #function for using stanford tagger to generate tags
+def tag_sents(fname, substr): 
+    find_sents(fname, substr)
+    
+    filename = (' ../../parse_tag_out/' + substr + '_search.txt')
+    
+    tagname = (' ../../parse_tag_out/' + substr + '_tag.txt')
+    
     import os
-    #this dir needs to be fixed when I get things running more smoothly
-    os.chdir('/Users/rschneid/Documents/Projects/zero/parse_tag')
-    os.getcwd()
-
-    #get user input
-    print('Please enter your substring')
-    substr = input()
-    print('Please enter your fileid')
-    fname = input()
-
-    ##pull out the lines corresponding to the word you're querying
-    import re
-    word_occur = []
-    pattern = re.compile(substr, re.IGNORECASE)
-    with open (fname, 'rt', encoding = 'latin1') as in_file:
-        for linenum, line in enumerate(in_file):
-            if pattern.search(line) !=None:
-                word_occur.append((line.rstrip('\n')))
-                
-    #save output to text file
-    filename = ('../parse_tag_out/' + substr + '_search.txt')
-    tagname = ('../parse_tag_out/' + substr + '_tag.txt')
+    os.chdir('/Users/rschneid/Documents/Projects/zero/parse_tag/tag')
+    cmd = ('./stanford-postagger.sh models/english-left3words-distsim.tagger' +
+           filename + ' > ' + tagname + ' 2>&1')
     
-    import numpy
-    numpy.savetxt(filename, word_occur, fmt = '%s')
-
-    #create a command to make os.system happy 
-    cmd = ('./stanford-postagger.sh models/english-left3words-distsim.tagger ' 
-           + filename + ' > ' + tagname + ' 2>&1')
-    
-    #actually run the command
+    import os
     os.system(cmd)
-    
-            
+ 
     
         
     
