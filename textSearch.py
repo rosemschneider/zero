b1f82c613bf51ca3ce649af82dcf7f5fabaec354
rschneid <rschneid@yalm01.psych.upenn.edu>
2017-06-12 13:30:26 -0400

    added function for pulling out all nums in one file

--- textSearch.py
+++ textSearch.py
@@ -17,6 +17,69 @@ def find_sents(fname, substr):
     import numpy
     numpy.savetxt(filename, word_occur, fmt = '%s')
     
+def find_nums_to_ten(fname):
+     nums_to_ten = 'zero|one|two|three|four|five|six|seven|eight|nine|ten'
+     
+     import re
+     word_occur = []
+     pattern = re.compile(nums_to_ten, re.IGNORECASE)
+     with open (fname, 'rt', encoding = 'latin1') as in_file:
+        for linenum, line in enumerate(in_file):
+            if pattern.search(line) !=None:
+                word_occur.append((line.rstrip('\n')))
+                
+     filename = ('parse_tag_out/' + 'num_to_ten' + '_search.txt')
+    
+     import numpy
+     numpy.savetxt(filename, word_occur, fmt = '%s')
+     
+def find_nums_teens(fname):
+     nums_teens = 'eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen'
+     
+     import re
+     word_occur = []
+     pattern = re.compile(nums_teens, re.IGNORECASE)
+     with open (fname, 'rt', encoding = 'latin1') as in_file:
+        for linenum, line in enumerate(in_file):
+            if pattern.search(line) !=None:
+                word_occur.append((line.rstrip('\n')))
+                
+     filename = ('parse_tag_out/' + 'nums_teens' + '_search.txt')
+    
+     import numpy
+     numpy.savetxt(filename, word_occur, fmt = '%s')
+
+def find_nums_decades(fname):
+     nums_decades = 'twenty|thirty|forty|fifty|sixty|seventy|eighty|ninety'
+     
+     import re
+     word_occur = []
+     pattern = re.compile(nums_decades, re.IGNORECASE)
+     with open (fname, 'rt', encoding = 'latin1') as in_file:
+        for linenum, line in enumerate(in_file):
+            if pattern.search(line) !=None:
+                word_occur.append((line.rstrip('\n')))
+                
+     filename = ('parse_tag_out/' + 'nums_decades' + '_search.txt')
+    
+     import numpy
+     numpy.savetxt(filename, word_occur, fmt = '%s')      
+     
+def find_nums_large(fname):
+     nums_large = 'hundred|thousand|million|billion|trillion'
+     
+     import re
+     word_occur = []
+     pattern = re.compile(nums_large, re.IGNORECASE)
+     with open (fname, 'rt', encoding = 'latin1') as in_file:
+        for linenum, line in enumerate(in_file):
+            if pattern.search(line) !=None:
+                word_occur.append((line.rstrip('\n')))
+                
+     filename = ('parse_tag_out/' + 'nums_large' + '_search.txt')
+    
+     import numpy
+     numpy.savetxt(filename, word_occur, fmt = '%s')      
 
 def parse_sents(fname, substr):
     find_sents(fname, substr)
