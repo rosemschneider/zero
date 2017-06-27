e8d28591b6febdf5f3d45d875e95d8759aff7a98
rschneid <rschneid@yalm01.psych.upenn.edu>
2017-06-27 12:19:09 -0400

    starting on multiple freq search

--- csvAnalysis.py
+++ csvAnalysis.py
@@ -27,6 +27,7 @@ def filterDF(df, column, substr):
     filtered = []
     # for row in data frame,
     for row in df[column]:
+        
         # for each key and value in the row,
         for key, value in row.items():
             if(type(substr) == tuple):
@@ -81,14 +82,55 @@ def freqsByAge(df, substr):
         age = int(age)
     return df
 
+#for multiple search terms
+def multFreqsByAge(df):
+    """This is a function to return the number of utterances a given word 
+    by age."""
+    empty_col = []   
+    for row in df['utterance']:
+        empty_col.append(textStats.getTypeFreq(row))   
+    freqs = pd.Series(empty_col)
+    #need to convert series to df to append
+    freqs = freqs.to_frame(name = 'typeFreqs')
+    df['typeFreqs'] = freqs['typeFreqs']
+    
+    ##filter down to only the words we care about
+    filtered_freqs = filterDF(df, 'typeFreqs', ('million', 'zero'))
+    return filtered_freqs
+    
+    #now get the actual values for only the substr you're interested in
+    filtered_freqs = filterDF(df, 'typeFreqs', substr)
+    #get the get the values
+    vals = []
+    for entry in filtered_freqs:
+        for value in entry.items():
+            vals.append(value[1])
+    #now that we have the values, append them to the df
+    num = pd.Series(vals)
+    num = num.to_frame(name = 'searchVals')
+    #add to the actual df
+    df['searchVals'] = num['searchVals']
+    
+    #finally, convert the age to numbers, drop the none
+    tmp_df = df
+    tmp_df = tmp_df[(tmp_df['age'] != '[None]')]
+    df = tmp_df
+    for age in df['age']:
+        age = int(age)
+    return df
+
+
 ##Framework for generating graphs
 #With zero input as example
 zero_input = readCSV('data/zero_input.csv')
+zero_output = readCSV('data/zero_output.csv')
+
+freqsByAge(zero_output, 'zero')
 
 ##For individual number words
 #First, get the freqs by age
 #group by age and aggregate by count
-grouped = zero_input_freqs[['age', 'searchVals']].groupby('age').agg('count')
+grouped = zero_output[['age', 'searchVals']].groupby('age').agg('count')
 
 #I need to learn more about pandas, but this is my clunky way of getting
 #values out of the grouped df
@@ -114,9 +156,9 @@ zero_input_freqs = freqsByAge(zero_input, 'zero')
 #Make a graph of the frequencies by age
 plt.bar(ages_int, values_int)
 plt.xlabel('Age (in months)', fontsize=12)
-plt.ylabel('Frequency in input', fontsize=12)
+plt.ylabel('Frequency in output', fontsize=12)
 plt.xticks(np.arange(min(ages_int), max(ages_int)+1, 10))
-plt.title('Zero frequency by age - input')
+plt.title('Zero frequency by age - output')
 
 #bigrams
 #get the utterances, and then convert to text
